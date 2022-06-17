74756dbe1b71c0598709882f97eb498665f2d51e
Idanturjeman <idan_turjeman29@walla.co.il>
2022-06-17 19:28:41 +0300

    some changes

--- mylibc.h
+++ mylibc.h
@@ -8,12 +8,12 @@ typedef struct myFILE
     int p;
 } myFILE, *p_myFILE;
 
-myFILE *myfopen(const char *pathname, const char *mode);
-int myfclose(myFILE *stream);
-size_t myfread(void *ptr, size_t size, size_t nmemb, myFILE *stream);
-size_t myfwrite(const void *ptr, size_t size, size_t nmemb, myFILE *stream);
-int myfseek(myFILE *stream, long offset, int whence);
-int myfscanf2(void *ptr, size_t size, size_t nmemb, myFILE *stream);
-int myfprintf2(int myfd, const void *buf, size_t count);
 int myfscanf(myFILE *stream, const char *format, ...);
 int myfprintf(myFILE *stream, const char *format, ...);
+size_t myfread(void *ptr, size_t size, size_t nmemb, myFILE *stream);
+int myfclose(myFILE *stream);
+int myfseek(myFILE *stream, long offset, int whence);
+myFILE *myfopen(const char *pathname, const char *mode);
+size_t myfwrite(const void *ptr, size_t size, size_t nmemb, myFILE *stream);
+int myfscanf2(void *ptr, size_t size, size_t nmemb, myFILE *stream);
+int myfprintf2(int myfd, const void *buf, size_t count);
