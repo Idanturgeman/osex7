575626243c844010be832fd4da70723cf1a1b2b6
Idanturjeman <idan_turjeman29@walla.co.il>
2022-06-17 19:22:31 +0300

    some changes

--- fs.h
+++ fs.h
@@ -41,22 +41,22 @@ struct disk_block {
 
 int myclose(int fd);
 void set_filesize(int filenum, int size);
-void create_fs(int s);
 int mymount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data);
-void sync_fs(const char* target);
 void print_fs();
 int myopendir(const char *name);
+void create_fs(int s);
 int allocate_file(int size, const char *name);
-size_t mywrite(int myfd, const void *buf, size_t count);
-int mylseek(int myfd, int offset, int whence);
+void sync_fs(const char* target);
 size_t myread(int myfd, void *buf, size_t count);
 struct superblock sb;
 void write_byte(int filenum,int pos, char* data);
 int get_block_num(int file, int offset);
 void shorten_file(int bn);
+size_t mywrite(int myfd, const void *buf, size_t count);
 struct open_file opened[12];
 char* name_disk;
 int myopen(const char *pathname, int flags);
+int mylseek(int myfd, int offset, int whence);
 void mymfks(int size);
 int myclosedir(int myfd);
 struct mydirent *myreaddir(int dirp);
